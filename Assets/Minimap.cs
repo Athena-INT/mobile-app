c1085786842716a0d438fd831be31651b66bdc49
Athena-INT <lyumin1025@gmail.com>
2017-08-19 09:29:20 +0800

    Update Minimap.cs

--- Assets/Minimap.cs
+++ Assets/Minimap.cs
@@ -6,3 +6,13 @@ public class Minimap : MonoBehaviour {
 
 	public Transform player;
 
+	void LateUpdate ()
+	{
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+
+		transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+	}
+
+}
