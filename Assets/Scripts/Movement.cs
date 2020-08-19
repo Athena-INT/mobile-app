5319985399ff2e99f606244c1cf68f517d5e9194
Athena-INT <lyumin1025@gmail.com>
2020-08-19 09:28:38 +0800

    Create Movement.cs

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 10f;
+	public float rotateSpeed = 10f;
+
+	private Vector3 movement;
+	private float rotation;
+
